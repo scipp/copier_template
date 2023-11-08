75dda105ac3dd126e6e7a17cc3d8833ade5aed2c
YooSunyoung <luysunyoung9@gmail.com>
2023-11-08 15:08:19 +0100

    Automatic testing of copier-template.

---
+++ test-template.sh
@@ -0,0 +1,18 @@
+pip install copier
+pip install tox
+
+DUMMYPROJECTPATH='./test-dummy'
+copier copy ./ ${DUMMYPROJECTPATH} \
+ -l \
+ -d description="Dummy project to test the template." \
+ -d projectname="testdummy" \
+ -d max_python="3.12" \
+ -d min_python="3.8" \
+ -d nightly_deps="" \
+ -d related_projects=""
+
+cd ${DUMMYPROJECTPATH}
+sed -i '/dependencies = \[/a\    "numpy"' pyproject.toml
+tox -e deps
+tox -e py39
+tox -e static
